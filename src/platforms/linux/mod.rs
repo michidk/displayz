484d20bb5dca9c26ccf9a5c58467f9e1831b5a61
Dom Rodriguez <shymega@shymega.org.uk>
2023-06-06 19:07:30 +0100

    Fix #[cfg] attributes on Linux platform

--- src/platforms/linux/mod.rs
+++ src/platforms/linux/mod.rs
@@ -1,12 +1,21 @@
 #[cfg(any(
-    target_os = "linux",
+    all(target_os = "linux", feature = "linux-x11"),
     target_os = "dragonfly",
     target_os = "freebsd",
     target_os = "netbsd",
     target_os = "openbsd"))]
 mod x11;
+
+#[cfg(any(
+    all(target_os = "linux", feature = "linux-x11"),
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd"))]
 pub use x11::*;
 
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", feature = "linux-wayland"))]
 mod wayland;
+
+#[cfg(all(target_os = "linux", feature = "linux-wayland"))]
 pub use wayland::*;
