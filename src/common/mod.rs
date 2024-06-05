38a0a20c3ddaec4e13ba8662b7508bf386c7ac87
Dom Rodriguez <shymega@shymega.org.uk>
2024-06-05 17:46:41 +0100

    refactor: Refactor types into structs, impl Debug/Display
    - Change `Resolutions` into tuple struct.
    - Impl `Debug` & `Display` for `Position` and `Resolutions`.
    - Use `Default` trait for default trait impl.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,16 +1,51 @@
 //! Common helpers for `displayz`
 
+use std::fmt::{self, Debug, Display, Formatter};
+
 /// Height and Width of a Display (`i32`)
 pub struct Resolution(i32, i32);
 
 /// X/Y positions of a display.
 pub struct Position(i32, i32);
 
-/// `Vec` type of the `Display` struct, exposed on a platform-dependent basis.
+/// `Vec` type of the `Display` type, exposed on a platform-dependent basis.
 pub type Displays = Vec<crate::Display>;
 
 /// `Vec` type of the `Resolution` type, generally exposing a collection of available resolutions.
-pub type Resolutions = Vec<Resolution>;
+pub struct Resolutions(Vec<Resolution>);
+
+impl Display for Resolution {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}x{}", self.0, self.1)
+    }
+}
+
+impl Debug for Resolution {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Resolution of Display is: {}x{}", self.0, self.1)
+    }
+}
+
+impl Debug for Resolutions {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(self.0.iter())
+            .finish()
+            .expect("Unable to format `Debug` output for `Resolutions` struct.");
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{},{}", self.0, self.1)
+    }
+}
+
+impl Debug for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Position of Display is: {},{}", self.0, self.1)
+    }
+}
 
 /// `DisplayOutput` defines the Trait specification of a platform's `Display`.
 /// A `Display` may contain:
@@ -23,11 +58,17 @@ pub trait DisplayOutput {
     /// Returns a boolean result, if the `Display` is currently active or not.
     fn is_active(&self) -> bool;
     /// Returns the `Position` custom type of the `Display`.
-    fn get_position(&self) -> Position;
+    fn get_position(&self) -> Position {
+        Position::default()
+    }
     /// Returns the current `Resolution` of the `Display`.
-    fn get_resolution(&self) -> Resolution;
+    fn get_resolution(&self) -> Resolution {
+        Resolution::default()
+    }
     /// Returns the current supported `Resolutions` of the `Display`.
-    fn get_supported_resolutions(&self) -> Resolutions;
+    fn get_supported_resolutions(&self) -> Resolutions {
+        Resolutions::default()
+    }
     /// Returns the EDID `&str` of the `Display.
     fn get_edid(&self) -> Option<&str>;
 }
